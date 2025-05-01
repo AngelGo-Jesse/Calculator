41ef1e1edd9ff74ddb51aa09aa07ec9219ac17a5
AngelO <angelojessedeguzmann@gmail.com>
2025-05-01 18:14:13 +0800

    Add prompt for Invalid expressions

--- Calculator.js
+++ Calculator.js
@@ -5,8 +5,14 @@
 const display = document.getElementById("display");
 let value = display.value; 
 
+// let lastInput = display.value.slice(-1);
+// const operators = ['+', '-', '*', '/'];
+
+
+
 function calculatorDisplay(input){
-    display.value += input;
+    display.value += input
+    // if (operators.includes(value) && operators.includes(lastInput)) {return;} else{display.value += input}
 }
 
 function clearDisplay(){
@@ -15,22 +21,25 @@ function clearDisplay(){
 
 
 function calculate(){
+    try{
     display.value = eval(display.value); 
 
-    if (eval(display.value) === 0){
-        alert("The number is Zero")
-        }if (display.value > 0){
-            if (display.value % 2 === 0){
-                alert("The number is positive & even")
-            }else{
-                alert("The number is positive & odd")
+        if (eval(display.value) === 0){
+            alert("The number is Zero")
+            }if (display.value > 0){
+                if (display.value % 2 === 0){
+                    alert("The number is positive & even")
+                }else{
+                    alert("The number is positive & odd")
+                }
+            }else {
+                if (display.value % 2 === 0){
+                    alert("The number is negative & even")
+                } else {
+                    alert("The number is negative & odd")}
             }
-        }else {
-            if (display.value % 2 === 0){
-                alert("The number is negative & even")
-            } else {
-                alert("The number is negative & odd")}
-        }
-    
+    } catch (error) {
+        alert("Error: Invalid Expression")
+    }
 }
 
