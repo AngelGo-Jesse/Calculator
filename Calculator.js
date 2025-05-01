fd19b17b538f31d9691ff1418b5de67d374f2511
AngelO <angelojessedeguzmann@gmail.com>
2025-05-01 21:18:06 +0800

    Add limit to input with operator

--- Calculator.js
+++ Calculator.js
@@ -1,18 +1,17 @@
 
 //calculator program
 //task; Show if number is {Positive.Even, Postive.Odd, Negative.Even , Negative.Odd} as an alert  
-
-const display = document.getElementById("display");
-let value = display.value; 
-
-// let lastInput = display.value.slice(-1);
-// const operators = ['+', '-', '*', '/'];
-
+const operators = ['+', '-', '*', '/'];
 
 
 function calculatorDisplay(input){
-    display.value += input
-    // if (operators.includes(value) && operators.includes(lastInput)) {return;} else{display.value += input}
+    const display = document.getElementById("display");
+    const lastInput = display.value.slice(-1);
+    
+    if (operators.includes(input) && operators.includes(lastInput)) {return;
+    }else {
+        display.value += input
+    }
 }
 
 function clearDisplay(){
