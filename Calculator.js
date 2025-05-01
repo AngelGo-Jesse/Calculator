773a55032e920edba688d59834496053d55a6d5f
AngelO <angelojessedeguzmann@gmail.com>
2025-05-02 00:27:50 +0800

    Bug fix, Add features and functional backspace button

--- Calculator.js
+++ Calculator.js
@@ -24,23 +24,31 @@ function calculate(){
     display.value = eval(display.value); 
     setTimeout(() => {
 
-        if (eval(display.value) === 0){
-            alert("The number is Zero")
-            }if (display.value > 0){
-                if (display.value % 2 === 0){
-                    alert("The number is positive & even")
-                }else{
-                    alert("The number is positive & odd")
-                }
-            }else {
-                if (display.value % 2 === 0){
-                    alert("The number is negative & even")
-                } else {
-                    alert("The number is negative & odd")}
-            }
+
     },50);
         } catch (error) {
             alert("Error: Invalid Expression")
         }
 }
 
+//new code
+function backspace(){
+    display.value = display.value.slice(0,-1);
+}
+
+// function alertButton(){
+//     if (eval(display.value) === 0){
+//         alert("The number is Zero")
+//         }if (display.value > 0){
+//             if (display.value % 2 === 0){
+//                 alert("The number is positive & even")
+//             }else{
+//                 alert("The number is positive & odd")
+//             }
+//         }else {
+//             if (display.value % 2 === 0){
+//                 alert("The number is negative & even")
+//             } else {
+//                 alert("The number is negative & odd")}
+//         }
+// }
